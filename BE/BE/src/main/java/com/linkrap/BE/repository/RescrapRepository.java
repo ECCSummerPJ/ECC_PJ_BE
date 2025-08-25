ab1bb81cad84d7e2f891ae0d45dd893616347418
jeein <23minjjj@ewha.ac.kr>
2025-08-25 12:25:44 +0900

    [Feat(link)] scrap delete 연관된 테이블 삭제 기능 추가

--- BE/BE/src/main/java/com/linkrap/BE/repository/RescrapRepository.java
+++ BE/BE/src/main/java/com/linkrap/BE/repository/RescrapRepository.java
@@ -1,7 +1,19 @@
 package com.linkrap.BE.repository;
 
 import com.linkrap.BE.entity.Rescrap;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface RescrapRepository extends CrudRepository<Rescrap, Integer>  {
+
+    @Modifying
+    //scrap 지워지면 해당 rescrap들도 지워지도록 하는 기능
+    @Query("""
+        delete
+        from Rescrap r
+        where r.scrap.scrapId=:scrapId
+    """)
+    void deleteRescrap(@Param("scrapId") Integer scrapId);
 }
